1119d5e471b93c46bccd287c5977eaee8cc18f4f
VaC306 <rishipursnani@gmail.com>
2023-02-27 21:32:56 +0100

    mas de hecho

--- PhysicsSimulatorTP/src/simulator/factories/BuilderBasedFactory.java
+++ PhysicsSimulatorTP/src/simulator/factories/BuilderBasedFactory.java
@@ -1,7 +1,11 @@
 package simulator.factories;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONObject;
 
@@ -11,26 +15,57 @@ import simulator.model.MovingBody;
 import simulator.model.MovingTowardsFixedPoint;
 import simulator.model.NewtonUniversalGravitation;
 
-public class BuilderBasedFactory<T> implements Factory{
+public class BuilderBasedFactory<T> implements Factory<T>{
 	
-	List<Builder<T>> builders;
+	private Map <String,Builder<T>> builders;
+	private List <JSONObject> buildersInfo;
 	
 	public BuilderBasedFactory()
 	{
-		
+		//create HashMap and linked list
+		builders = new HashMap<> ();
+		buildersInfo = new LinkedList<>();
 	}
 	
 	public BuilderBasedFactory(List<Builder<T>> builders)
 	{
-		 this.builders = builders;
+		//creation of hashmaps y lista
+		this();
+		// add builder for each builder
+		for(Builder<T> b: builders) 
+		{	
+			addBuilder(b);
+		}
 	}
 	
 	@Override
 	public T createInstance(JSONObject info){
-	    String type = info.getString("type");
-	    JSONObject data = info.getJSONObject("data");
+	    
+	  	if (info == null) 
+	  	{
+	  		throw new IllegalArgumentException("Invalid value for createInstance:null");
+	  	}
+	  		
+	  	//builder no tiene type, no se puede construir
+	  	if(!builders.containsKey(info.getString("type")) ) 
+	  	{		
+	  		throw new IllegalArgumentException("Invalid value for createInstance: " + info.toString());	
+	  	}
 
-	    switch (type) {
+	  	JSONObject data;
+
+	  	if(info.has("data")) 
+	  	{
+	  		data = info.getJSONObject("data");
+	  	}
+	  	else 
+	  	{		
+	  		data=new JSONObject();
+	  	}
+	  	
+	  	return builders.get(info.getString("type")).createInstance(data);
+	    
+	   /*switch (type) {
 	        case "Body":
 	            String id = data.getString("id");
 	            String gid = data.getString("gid");
@@ -63,22 +98,19 @@ public class BuilderBasedFactory<T> implements Factory{
 	            return (T) new NewtonUniversalGravitation(G);
 	        default:
 	            throw new IllegalArgumentException("Invalid object type: " + type);
-	    }
+	    }*/
 	}
 	
 	
 	@Override
 	public List<JSONObject> getInfo() {
-	    List<JSONObject> infoList = new ArrayList<>();
-	    for (Builder<T> builder : builders) {
-	        infoList.add(builder.getInfo());
-	    }
-	    return infoList;
+		return Collections.unmodifiableList(buildersInfo);
 	}
 	
 	void addBuilder(Builder<T> b)
 	{
-		builders.add(b);
+		builders.put(b.getTypeTag(), b); //we save the constructor type
+		buildersInfo.add(b.getInfo()); // add b.getInfo () to buildersInfo
 	}
 
 }
--- PhysicsSimulatorTP/src/simulator/factories/NoForceBuilder.java
+++ PhysicsSimulatorTP/src/simulator/factories/NoForceBuilder.java
@@ -5,7 +5,12 @@ import org.json.JSONObject;
 import simulator.model.NoForce;
 
 public class NoForceBuilder extends Builder<NoForce>{
-
+	
+	public NoForceBuilder()
+	{
+		super("nf", "a");
+	}
+	
 	public NoForceBuilder(String typeTag, String desc) {
 		super(typeTag, desc);
 	}
